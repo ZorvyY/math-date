a3629367d3d4babdfafdb1d7883ae6329857031d
Zoravur Singh <zoravur.s@gmail.com>
2018-03-11 22:47:40 -0400

    test new expressions

--- scripts/expressions.js
+++ scripts/expressions.js
@@ -9,7 +9,7 @@ for (let i = 1; i <= 31; i++) {
     expressions[i] = (`$$\\sqrt{${(i+1)**2}}-\\sqrt{1}\\$$`);
 }
 
-require('fs').writeFileSync('expressions.json', JSON.stringify(expressions, 0, 2));
+require('fs').writeFileSync('../temp/expressions.json', JSON.stringify(expressions, 0, 2));
 
 
 module.exports = expressions;
