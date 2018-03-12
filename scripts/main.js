a455b0922e1bc8674d14d808fa0704a4bfcc6c31
Zoravur Singh <zoravur.s@gmail.com>
2018-03-11 23:12:58 -0400

    changed fetch url

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 //var expressionList = require("./expressions.js");
 
 async function loadExpression () {
-  var data = await fetch('https://zorvyy.github.io/math-date/temp/expressions.json')
+  var data = await fetch('https://zorvyy.github.io/math-date/expressions.json')
   .then(response => response.text());
   console.log(data);
   let expressionList = JSON.parse(data);
