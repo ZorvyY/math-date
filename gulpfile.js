176b57edf0ef4d0b0660622053e0caf89d02770a
Zoravur Singh <zoravur.s@gmail.com>
2018-03-12 22:01:17 -0400

    Edited styling

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 gulp.task('expressions', function() {
   let expressions = {};
   for (let i = 1; i <= 31; i++) {
-      expressions[i] = (`$\\sqrt{${(i+1)**2}}-\\sqrt{1}\\$`);
+      expressions[i] = (`\\sqrt{${(i+1)**2}}-\\sqrt{1}`);
   }
   require('fs').writeFileSync('expressions.json', JSON.stringify(expressions, 0, 2));
 });
---
+++ scripts/backgroundLoad.js
@@ -0,0 +1,2 @@
+var body = document.getElementById('body');
+body.style.backgroundImage = `url('https://picsum.photos/${body.style.width}/${body.style.height}/?random&blur')`;
